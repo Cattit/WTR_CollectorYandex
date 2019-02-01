6cf29ffbab5be9f367a0f29cd912c005a2c9efd5
Cattit <ekaterina.titaeva@gmail.com>
2019-02-01 14:32:35 +0800

    fix processing of data

--- data/yandex.js
+++ data/yandex.js
@@ -1,119 +1,130 @@
-const axios = require("axios")
-let dal = require ('wtr-dal') 
+const axios = require("axios");
+let dal = require("wtr-dal");
+let source = "yandex"
+const dateNow = new Date()
 
-function rainfall(type){
-  switch(type) {
-    case 0:  // if (x type=== 0)
-      return "not"
-      break
-    case 1:  
-      return "rain"
-      break
-    case 2:  
-      return "rain/snow"
-      break
-    case 3:  
-      return "snow"
-      break 
-
-    default:
-      console.log("ERROR rainfall yandex")
-      return "error"
-      break
+function dateDay(amount_day) {
+  return {
+    now: dateNow,
+    date_start: new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + amount_day, 12),
+    date_end: new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + amount_day, 23, 59, 59, 999),
+    type: "day"
   }
 }
 
-function rainfall_strength(type){
-  switch(type) {
-    case 0:  // if (x type=== 0)
-      return "not"
-      break
-    case 0.25:  
-      return "light"
-      break
-    case 0.5:  
-      return "norm"
-      break
-    case 0.75:  
-    case 1: 
-      return "heavy"
-      break 
-
-    default:
-      console.log("ERROR rainfall_strength yandex")
-      return "error"
-      break
+function dateNight(amount_day) {
+  return {
+    now: dateNow,
+    date_start: new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + amount_day, 00),
+    date_end: new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + amount_day, 11, 59, 59, 999),
+    type: "night"
   }
 }
 
-function cloudness(type){
-  switch(type) {
-    case 0:  // if (x type=== 0)
-      return "not"
-      break
-    case 0.25: 
-    case 0.5:  
-    case 0.75: 
-      return "light"
-      break  
-    case 1: 
-      return "heavy"
-      break 
-
-    default:
-      console.log("ERROR cloudness yandex")
-      return "error"
-      break
+function rainfall(weather) {  // не возвоащает инфу о яп, только о дожде, снеге и снеге с дождем
+  return {
+    snow: weather === 3 ? 1 : 0,
+    rain: weather === 1 ? 1 : 0,
+    sand: 0,
+    squall: 0,
+    mist: 0,
+    storm: 0,
+    drizzle: 0,
+    rainsnow: weather === 2 ? 1 : 0
   }
 }
 
-function analyzeData(newdata, newlat, newlon){
-  let dataAll = new Map();
+function rainfall_strength(type) {
+  switch (type) {
+    case 0: // if (x type=== 0)
+      return "not";
+      break;
+    case 0.25:
+      return "light";
+      break;
+    case 0.5:
+      return "norm";
+      break;
+    case 0.75:
+    case 1:
+      return "heavy";
+      break;
 
-  for (let dd = 1, i = 0; dd<6; dd+=2, i++){
-    let data1 = new Object(); 
-    data1.lat = newlat;
-    data1.lon = newlon;
-    data1.date = newdata.forecasts[dd].date;
-    data1.daytime = "day";
-    data1.temperature = newdata.forecasts[dd].parts.day_short.temp;
-    data1.wind_speed = newdata.forecasts[dd].parts.day_short.wind_speed;
-    data1.wind_gust = newdata.forecasts[dd].parts.day_short.wind_gust;
-    // data1.wind_direction = newdata.forecasts[dd].parts.day_short.wind_dir;
-    // data1.pressure = newdata.forecasts[dd].parts.day_short.pressure_mm;
-    data1.rainfall = rainfall(newdata.forecasts[dd].parts.day_short.prec_type);
-    data1.rainfall_mm = newdata.forecasts[dd].parts.day_short.prec_mm;
-    // data1.rainfall_strength = rainfall_strength(newdata.forecasts[dd].parts.day_short.prec_strength); 
-    // data1.cloudness = cloudness(newdata.forecasts[dd].parts.day_short.cloudness); 
-    // data1.humidity = newdata.forecasts[dd].parts.day_short.humidity;
-    dataAll.set(i, data1); 
-    i++;
-
-    let data2 = new Object(); 
-    data2.lat = newlat;
-    data2.lon = newlon;
-    data2.date = newdata.forecasts[dd].date;
-    data2.daytime = "night";
-    data2.temperature = newdata.forecasts[dd].parts.night_short.temp;
-    data2.wind_speed = newdata.forecasts[dd].parts.night_short.wind_speed;
-    data2.wind_gust = newdata.forecasts[dd].parts.night_short.wind_gust;
-    // data2.wind_direction = newdata.forecasts[dd].parts.night_short.wind_dir;
-    // data2.pressure = newdata.forecasts[dd].parts.night_short.pressure_mm;
-    data2.rainfall = rainfall(newdata.forecasts[dd].parts.night_short.prec_type);
-    data2.rainfall_mm = newdata.forecasts[dd].parts.night_short.prec_mm;
-    // data2.rainfall_strength = rainfall_strength(newdata.forecasts[dd].parts.night_short.prec_strength); 
-    // data2.cloudness = cloudness(newdata.forecasts[dd].parts.night_short.cloudness); 
-    // data2.humidity = newdata.forecasts[dd].parts.night_short.humidity;
-    dataAll.set(i, data2);
+    default:
+      console.log("ERROR rainfall_strength yandex");
+      return "error";
+      break;
   }
-  //  console.log(dataAll)
+}
 
-  dal.saveMeteoData(dataAll) 
-  // .then((result)=> { // !
-  //     console.log("Successfully sent data from Yandex")
-  // }, (error)=> {
-  //     console.log("Error sending data from Yandex")  
-  // });
+function cloudness(type) {
+  switch (type) {
+    case 0: // if (x type=== 0)
+      return "not";
+      break;
+    case 0.25:
+    case 0.5:
+    case 0.75:
+      return "light";
+      break;
+    case 1:
+      return "heavy";
+      break;
+
+    default:
+      console.log("ERROR cloudness yandex");
+      return "error";
+      break;
+  }
+}
+
+async function analyzeData({ info, forecasts }) {
+  let dataAll = [];
+  const { lat, lon } = info;
+  for (let dd = 1, i = 0; dd < 6; dd += 2, i++) {
+    dataAll.push(
+      {
+        source,
+        lat,
+        lon,
+        depth_forecast: dd,
+        date: dateDay(dd),
+        temperature: forecasts[dd].parts.day_short.temp,
+        wind_speed: {
+          from: forecasts[dd].parts.day_short.wind_speed,
+          to: forecasts[dd].parts.day_short.wind_speed
+        },
+        wind_gust: forecasts[dd].parts.day_short.wind_gust,
+        rainfall: rainfall(forecasts[dd].parts.day_short.prec_type),
+        amount_rainfall: forecasts[dd].parts.day_short.prec_mm
+      }
+    )
+
+    dataAll.push(
+      {
+        source,
+        lat,
+        lon,
+        depth_forecast: dd,
+        date: dateNight(dd),
+        temperature: forecasts[dd].parts.night_short.temp,
+        wind_speed: {
+          from: forecasts[dd].parts.night_short.wind_speed,
+          to: forecasts[dd].parts.night_short.wind_speed
+        },
+        wind_gust: forecasts[dd].parts.night_short.wind_gust,
+        rainfall: rainfall(forecasts[dd].parts.night_short.prec_type),
+        amount_rainfall: forecasts[dd].parts.night_short.prec_mm
+      }
+    )
+
+  }
+  // console.log(dataAll)
+  await dal.saveForecast(source, dateNow);
+  Promise.all(dataAll.map(forecast => dal.saveMeteoData(forecast)))
+    .then(() => console.log("Successfully sent data from Yandex"))
+    .catch(error => console.log("Error sending data from Yandex"))
+
 }
 
 function getforecast(newlat, newlon) {
@@ -124,17 +135,17 @@ function getforecast(newlat, newlon) {
       "X-Yandex-API-Key": "5e758c97-9f5b-4f63-a72b-b5c4e0624e90"
     },
     params: {
-      lat: newlat, 
+      lat: newlat,
       lon: newlon,
       lang: "ru_RU",
-      limit: 7, 
+      limit: 7,
       hours: true,
       extra: true
     }
   })
     .then(res => {
       // console.log(res.data);
-      analyzeData(res.data, newlat, newlon);
+      analyzeData(res.data);
     })
     .catch(err => console.log(err));
 }
--- index.js
+++ index.js
@@ -1,24 +1,2 @@
-var express = require('express')
-var cors = require('cors')
-var morgan = require('morgan')
-var bodyParser = require('body-parser')
-const axios = require("axios")
-let dal = require ('wtr-dal'); 
-
-const db = require('./db')
-var app = express()
-app.use(cors())
-app.use(morgan('combined'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000, function(){
-    console.log("PORT 3000!")
-})
-
 let getData = require("./data/yandex.js"); 
-getData.getforecast(55.75396, 37.620393);
+getData.getforecast(52.29778, 104.29639);
